bca6a418070814d8dd1b2cdb89f9153e4665ca2c
bukhuti-vardanidze <vardanidzebuta6@gmail.com>
2022-03-21 18:47:56 +0400

    add Anim/Respons

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,27 @@
-  let burger = document.getElementById('burger');
-  let navig = document.getElementById('navigation');
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
 
-  
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+})
+
+document.querySelector('.nav-link').forEach( n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+
+} ))
+
+
+/*animation */
+LottieInteractivity.create({
+  mode: 'scroll',
+  player: '#firstLottie',
+  actions: [
+      {
+        visibility: [0,1],
+        type: 'seek',
+        frames: [0, 900],
+      },
+    ],
+});
